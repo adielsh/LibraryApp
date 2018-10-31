fdafc53646d5e29f2091b5be30635826616ebf2b
adielsh <adielvl@gmail.com>
2018-10-31 12:43:45 +0200

    add electron wrapping

---
+++ client/src/renderer/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+	<Library></Library>
+  </div>
+</template>
+
+<script>
+  import Library from '@/components/Library'
+  export default {
+    name: 'library',
+    components: {
+	  Library
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
--- client/src/renderer/main.js
+++ client/src/renderer/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+import App from './App'
+
 import BootstrapVue from 'bootstrap-vue'
 Vue.use(BootstrapVue);
-Vue.config.productionTip = false
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App)
+  components: { App },
+  template: '<App/>'
 }).$mount('#app')
